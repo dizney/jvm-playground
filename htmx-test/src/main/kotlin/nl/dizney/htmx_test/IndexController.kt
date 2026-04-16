061cb21ee67a2152d5ba2f4359cb752b6fdb9db2
Sidney Beekhoven <903673+dizney@users.noreply.github.com>
2026-04-16 16:17:39 +0200

    add search

--- htmx-test/src/main/kotlin/nl/dizney/htmx_test/IndexController.kt
+++ htmx-test/src/main/kotlin/nl/dizney/htmx_test/IndexController.kt
@@ -8,12 +8,25 @@ import java.time.LocalDateTime
 
 @Controller
 class IndexController {
-   @GetMapping("/")
-   fun index() = "index"
+    companion object {
+        val searchResults = listOf("one", "two", "three", "four", "five")
+    }
+
+    @GetMapping("/")
+    fun index() = "index"
 
     @PostMapping("/clicked")
     fun clicked(model: Model): String {
         model.addAttribute("now", LocalDateTime.now().toString())
         return "clicked :: result"
+    }
+
+    @GetMapping("/search")
+    fun search(q: String, model: Model): String {
+        model.addAttribute(
+            "results",
+            searchResults.filter { it.startsWith(q.lowercase()) }
+        )
+        return "search :: results"
     }
 }
